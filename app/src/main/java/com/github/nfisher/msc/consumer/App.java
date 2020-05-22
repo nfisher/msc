9a03b7dcab1d76f1fe6baf3c64fc14e8292be946
Nathan Fisher <nfisher@junctionbox.ca>
2020-05-22 20:13:53 -0300

    Initial import with single message

---
+++ app/src/main/java/com/github/nfisher/msc/consumer/App.java
@@ -0,0 +1,20 @@
+package com.github.nfisher.msc.consumer;
+
+import static com.github.nfisher.msc.proto.Metrics.Metric;
+
+import java.time.Instant;
+
+public class App {
+    public static void main(String []args) {
+        Metric.Builder builder = Metric.newBuilder();
+        builder
+            .setTimestamp(Instant.now().toEpochMilli())
+            .addValues(1.0)
+            .addValues(2.0)
+            .addValues(3.0);
+
+        Metric metric = builder.build();
+
+        System.out.println(metric.toString());
+    }
+}
