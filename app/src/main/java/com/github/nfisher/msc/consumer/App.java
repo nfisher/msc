0c141f17d7ee77919eef70e4c2ba2746fced0e4f
Nathan Fisher <nfisher@junctionbox.ca>
2020-05-22 22:48:05 -0300

    Add uid field to Metric

--- app/src/main/java/com/github/nfisher/msc/consumer/App.java
+++ app/src/main/java/com/github/nfisher/msc/consumer/App.java
@@ -8,6 +8,7 @@ public class App {
     public static void main(String []args) {
         Metric.Builder builder = Metric.newBuilder();
         builder
+            .setUid(543212345)
             .setTimestamp(Instant.now().toEpochMilli())
             .addValues(1.0)
             .addValues(2.0)
